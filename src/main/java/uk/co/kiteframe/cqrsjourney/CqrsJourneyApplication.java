65c78e0304f4fbb05121d9a8b951d658f0932c98
Tim Mortimer <tim.mortimer@level5.co.uk>
2021-09-10 16:45:59 +0100

    Initial commit

---
+++ src/main/java/uk/co/kiteframe/cqrsjourney/CqrsJourneyApplication.java
@@ -0,0 +1,13 @@
+package uk.co.kiteframe.cqrsjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsJourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsJourneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/co/kiteframe/cqrsjourney/CqrsJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.kiteframe.cqrsjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsJourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
