c8e64bd63897fbccb4f3c1da9b4fbd14c2e502f5
Tim Mortimer <tim.mortimer@level5.co.uk>
2021-09-15 13:20:37 +0100

    Create HTTP endpoint for retrieving a conference by its code

---
+++ src/main/java/uk/co/kiteframe/cqrsjourney/conferencemanagement/ConferenceModel.java
@@ -0,0 +1,4 @@
+package uk.co.kiteframe.cqrsjourney.conferencemanagement;
+
+record ConferenceModel(String code, String title, String description) {
+}
--- src/test/java/uk/co/kiteframe/cqrsjourney/conferencemanagement/CreateConferenceControllerTest.java
+++ src/test/java/uk/co/kiteframe/cqrsjourney/conferencemanagement/CreateConferenceControllerTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(CreateConferenceController.class)
 public class CreateConferenceControllerTest {
 
     @Autowired
